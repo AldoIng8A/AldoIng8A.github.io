032fe559b3b833f7109059fe27b4ae4ba7cd23ca
AldoIng8A <157191187+AldoIng8A@users.noreply.github.com>
2024-01-23 12:13:34 -0600

    Create login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+	console.log("Hello, world!");
+	var inputEmail = document.getElementsById('email');
+	var inputPassword = document.getElementsById('password');
+	console.log(inputEmail);
+	console.log(inputPassword);
+});
